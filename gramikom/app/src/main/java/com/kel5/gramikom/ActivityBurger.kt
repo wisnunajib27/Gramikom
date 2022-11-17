9dd556d547e5ab4a28cefb37df0cdd3977ab5a0e
wisnunajib27 <108852555+wisnunajib27@users.noreply.github.com>
2022-11-17 09:36:08 +0700

    Add files via upload

---
+++ gramikom/app/src/main/java/com/kel5/gramikom/ActivityBurger.kt
@@ -0,0 +1,11 @@
+package com.kel5.gramikom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityBurger : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_burger)
+    }
+}
